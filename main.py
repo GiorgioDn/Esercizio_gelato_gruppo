e369312f905863392963ea24114e93ec0de628f7
annafirinu <anna.firinu2@outlook.it>
2025-11-19 17:15:02 +0100

    main prima modifica
    main prima modifica

---
+++ main.py
@@ -0,0 +1,69 @@
+from moduli.gusto import Gusto
+from moduli.gustopremium import GustoPremium
+
+def menu():
+    print("\nMENU GELATERIA ")
+    print("1. Aggiungi gusto base")
+    print("2. Aggiungi gusto premium")
+    print("3. Mostra tutti i gusti")
+    print("4. Esci")
+    return input("Scegli un'opzione: ")
+
+def chiedi_allergeni():
+    allergeni = input("Inserisci allergeni separati da virgola (oppure lascia vuoto): ")
+    if allergeni.strip() == "":
+        return []
+    return [a.strip() for a in allergeni.split(",")]
+
+def chiedi_lista(messaggio):
+    testo = input(messaggio + " (separati da virgola): ")
+    return [x.strip() for x in testo.split(",")]
+
+def main():
+    gusti = []  # lista che contiene tutti i gusti creati
+
+    while True:
+        scelta = menu()
+
+        if scelta == "1":
+            print("\nNUOVO GUSTO BASE")
+            nome = input("Nome: ")
+            prezzo = float(input("Prezzo base: "))
+            allergeni = chiedi_allergeni()
+
+            gusto = Gusto(nome, prezzo, allergeni)
+            gusti.append(gusto)
+            print("Gusto base aggiunto!")
+
+        elif scelta == "2":
+            print("\nNUOVO GUSTO PREMIUM")
+            nome = input("Nome: ")
+            prezzo = float(input("Prezzo base: "))
+            allergeni = chiedi_allergeni()
+            ingredienti = chiedi_lista("Ingredienti speciali")
+            sovrapprezzo = float(input("Sovrapprezzo: "))
+
+            gusto_p = GustoPremium(nome, prezzo, allergeni, ingredienti, sovrapprezzo)
+            gusti.append(gusto_p)
+            print("Gusto premium aggiunto!")
+
+        elif scelta == "3":
+            print("\nLISTA DEI GUSTI")
+            if len(gusti) == 0:
+                print("Nessun gusto presente")
+            else:
+                for g in gusti:
+                    if isinstance(g, GustoPremium):
+                        print(g.descrizione_premium())
+                    else:
+                        print(g.descrizione())
+
+        elif scelta == "4":
+            print("Arrivederci")
+            break
+
+        else:
+            print("Scelta non valida, riprova")
+
+if __name__ == "__main__":
+    main()
