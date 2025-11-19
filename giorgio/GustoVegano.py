44dec5c6a63a9e515babc68b1779a9f88d013fae
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-19 16:23:19 +0100

    Aggiunto gusto vegano ed esempio idea modulo

---
+++ giorgio/GustoVegano.py
@@ -0,0 +1,25 @@
+from .module.Gusto import * 
+
+class GustoVegano(Gusto):
+    #prende i dati della super classe gusto in aggiunta alla stringa base_vegetale
+    def __init__(self, nome:str, prezzo_base:float, allergeni:list[str], base_vegetale:str):
+        super().__init__(nome, prezzo_base, allergeni)
+        self.__base_vegetale=base_vegetale
+    
+    #restituisco il parametro privato base_vegetale 
+    def get_base_vegetale(self):
+        if len(self.__base_vegetale) > 0:
+            return self.__base_vegetale
+        else:
+            return False
+    
+    def set_base_vegetale(self, base_vegetale):
+        if len(self.__base_vegetale) > 0:
+            self.__base_vegetale = base_vegetale
+        else:
+            return False
+    
+    
+    def descrizione(self):
+        base = super().descrizione()
+        return f"Il gelato {base} ha la base vegetale al {self.__base_vegetale}"
